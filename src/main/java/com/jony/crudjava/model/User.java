1ab997a6d825b6cecfdbfce092f6ac81358343fe
Jonatas <jonatasfontele@gmail.com>
2022-08-02 18:31:26 -0300

    feat: User entity created

---
+++ src/main/java/com/jony/crudjava/model/User.java
@@ -0,0 +1,41 @@
+package com.jony.crudjava.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private String role;
+
+    private String office;
+
+    private LocalDate admissionDate;
+
+    private LocalDate expirationDate;
+
+}
